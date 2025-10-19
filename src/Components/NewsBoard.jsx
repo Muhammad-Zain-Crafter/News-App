59e58d5fb301b753432e41db6f3884d0315b47bb
Muhammad-Zain-Crafter <zmunawar872@gmail.com>
2025-10-19 10:42:22 -0700

    style: format NewsBoard component for improved readability

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,31 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import NewsItem from './NewsItem';
+import React, { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
 
-const NewsBoard = ({category}) => {
-     const [articles, setArticle] = useState([]);
-    useEffect(() => {
-    const key = '821593c6d3711eb2403ccfaf4cc67e37';
+const NewsBoard = ({ category }) => {
+  const [articles, setArticle] = useState([]);
+  useEffect(() => {
+    const key = import.meta.env.VITE_API_KEY;
     let url = `https://gnews.io/api/v4/top-headlines?lang=en&country=us&topic=${category}&token=${key}`;
-    fetch(url).then(response => response.json()).then(data => setArticle(data.articles));
-    }, [category]);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticle(data.articles));
+  }, [category]);
   return (
     <div>
       <div>
-         <h1 className='font-medium mt-2 text-center text-xl '>Latest <span className='bg-red-600 text-white rounded p-0.5'>News</span></h1>
+        <h1 className="font-medium mt-2 text-center text-xl ">
+          Latest{" "}
+          <span className="bg-red-600 text-white rounded p-0.5">News</span>
+        </h1>
       </div>
 
-      <div className='flex flex-wrap justify-center gap-6 p-6'>
-          {/* API attributes:
+      <div className="flex flex-wrap justify-center gap-6 p-6">
+        {/* API attributes:
           title
           description
           image
           url */}
-          {articles.map((news, index) => {
-              return <NewsItem key={index} title={news.title} description={news.description} src={news.image} url={news.url} />
-          })}
+        {articles.map((news, index) => {
+          return (
+            <NewsItem
+              key={index}
+              title={news.title}
+              description={news.description}
+              src={news.image}
+              url={news.url}
+            />
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NewsBoard
+export default NewsBoard;
